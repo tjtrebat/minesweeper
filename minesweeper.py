d046b5d9c7b0b45c20cb72321af8d641253f685d
Tom Trebat <tjtrebat@gmail.com>
2011-02-12 12:46:40 -0500

    added custom options

--- minesweeper.py
+++ minesweeper.py
@@ -5,14 +5,13 @@ from Tkinter import *
 from PIL import Image, ImageTk
 
 class Minesweeper:
-    def __init__(self, root, mines=10, size=9):
+    def __init__(self, root):
         self.root = root
         self.root.title("Minesweeper")
         self.frame = Frame(root)
         self.frame.grid()
-        self.size = size
-        self.num_mines = mines
-        self.board = {}
+        self.size = (9,) * 2
+        self.num_mines = 10
         self.buttons = {}
         self.add_menu_bar()
         self.add_header()
@@ -22,7 +21,14 @@ class Minesweeper:
         menu = Menu(self.root)
         file_menu = Menu(menu, tearoff=0)
         file_menu.add_command(label="New", command=self.new_game)
-        #file_menu.add_command(label="Open", command=self.open_note)
+        self.level = "Beginner"
+        self.levels = {"Beginner": BooleanVar(), "Intermediate": BooleanVar(), "Advanced": BooleanVar(), "Custom": BooleanVar()}
+        file_menu.add_checkbutton(label="Beginner", variable=self.levels["Beginner"], command=lambda x= "Beginner":self.new_game(level=x))
+        file_menu.add_checkbutton(label="Intermediate", variable=self.levels["Intermediate"], command=lambda x= "Intermediate":self.new_game(level=x))
+        file_menu.add_checkbutton(label="Advanced", variable=self.levels["Advanced"], command=lambda x= "Advanced":self.new_game(level=x))
+        file_menu.add_command(label="Custom", command=self.custom_level)
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=quit)
         menu.add_cascade(label="File", menu=file_menu)
         self.root.config(menu=menu)
 
@@ -38,7 +44,15 @@ class Minesweeper:
         self.tv_mines.set(self.num_mines)
         Label(frame, textvariable=self.tv_mines).grid(row=0, column=3)
 
-    def new_game(self):
+    def new_game(self, level=None):
+        if level is not None:
+            self.levels[self.level].set(False)
+            self.level = level
+            self.size = self.get_size()
+            self.num_mines = self.get_num_mines()
+            if self.level == "Custom":
+                self.custom.destroy()
+        self.levels[self.level].set(True)
         self.mines = self.get_mines()
         self.flags = []
         self.questions = []
@@ -47,13 +61,41 @@ class Minesweeper:
         if hasattr(self, "timer"):
             self.tv_timer.set(0)
             self.time.after_cancel(self.timer)
-        print self
+
+    def custom_level(self):
+        self.custom = Tk()
+        frame = Frame(self.custom, padx=10, pady=10)
+        frame.grid()
+        Label(frame, text="Height:").grid(row=0, column=0)
+        self.custom_height = Spinbox(frame, width=3, from_=9, to=24)
+        self.custom_height.grid(row=0, column=1)
+        Label(frame, text="Width:").grid(row=1, column=0)
+        self.custom_width = Spinbox(frame, width=3, from_=9, to=30)
+        self.custom_width.grid(row=1, column=1)
+        Label(frame, text="Mines:").grid(row=2, column=0)
+        self.custom_mines = Spinbox(frame, width=3, from_=10, to=668)
+        self.custom_mines.grid(row=2, column=1)
+        Button(frame, text="OK", command=lambda x= "Custom":self.new_game(level=x)).grid()
+
+    def get_size(self):
+        sizes = {"Beginner": (9, 9), "Intermediate": (16, 16), "Advanced": (16, 30)}
+        if self.level == "Custom":
+            return (int(self.custom_height.get()), int(self.custom_width.get()))
+        return sizes[self.level]
+
+    def get_num_mines(self):
+        mines = {"Beginner": 10, "Intermediate": 40, "Advanced": 99}
+        if self.level == "Custom":
+            return int(self.custom_mines.get())
+        return mines[self.level]
 
     def add_board(self):
+        self.board = {}
         for key in self.buttons:
             self.buttons[key].destroy()
-        for i in range(self.size):
-            for j in range(self.size):
+        self.buttons = {}
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
                 key = (i, j)
                 if key in self.mines:
                     self.board[key] = 'm'
@@ -61,6 +103,7 @@ class Minesweeper:
                 else:
                     self.board[key] = str(self.get_mine_count(key))
                     self.add_button(key, width=1, height=1, command=lambda x=key:self.start_game(x))
+        print self
 
     def start_game(self, space):
         self.tick()
@@ -115,7 +158,7 @@ class Minesweeper:
     def get_mines(self):
         mines = []
         while len(mines) < self.num_mines:
-            mine = (random.randint(0, self.size - 1), random.randint(0, self.size - 1))
+            mine = (random.randint(0, self.size[0] - 1), random.randint(0, self.size[1] - 1))
             if mine not in mines:
                 mines.append(mine)
         return mines
@@ -150,8 +193,8 @@ class Minesweeper:
         self.buttons[key].grid(row=key[0], column=key[1])
 
     def found_mine(self):
-        for i in range(self.size):
-            for j in range(self.size):
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
                 key = (i, j)
                 if self.board[key] == 'm':
                     self.buttons[key].destroy()
@@ -174,8 +217,8 @@ class Minesweeper:
     def try_game_over(self):
         num_btn = 0
         mines_found = 0
-        for i in range(self.size):
-            for j in range(self.size):
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
                 if isinstance(self.buttons[(i, j)], Button):
                     num_btn += 1
                     if self.board[(i, j)] == 'm' and (i, j) in self.flags:
@@ -189,8 +232,8 @@ class Minesweeper:
 
     def __str__(self):
         s = ""
-        for i in range(self.size):
-            for j in range(self.size):
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
                 s += self.board[(i, j)] + "\t"
             s += "\n"
         return s
